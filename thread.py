ee66f94d4bc7a0cb0ebbc898382831640d30158e
szyszy315 <zeyusong@bu.edu>
2020-02-21 11:42:43 -0500

    Update thread.py

--- thread.py
+++ thread.py
@@ -5,30 +5,34 @@ from texttoimage import *
 from image2video import *
 
 
-def thread():
-    cnt = 0 
-    while True:
-        if q.empty():
-            break
-        work = q.get()
-        cnt += 1
-        print("working on {}th thread".format(cnt))
-        get_all_tweets(work)
-        image(work)
-        video(work)
-        q.task_done()
+def main(tweets):
+    def thread():
+        while True:
+            if q.empty():
+                break
+            work = q.get()
+            print('='*30)
+            print("working on {} thread".format(work))
+            get_all_tweets(work)
+            image(work)
+            video(work)
+            print('='*30)
+            print("{} thread done".format(work))
+            q.task_done()
+        print('all works done')
+    q = Queue()
+    for i in tweets:
+        q.put(i)
+    num_threads = 3
+    threads = []
+    for i in range(num_threads):
+        t = threading.Thread(target=thread)
+        t.daemon = True
+        threads.append(t)
+        t.start()
+    q.join() 
 
-
-tweets = ["@BU_Tweets","@BUAthletics","@BUexperts"]
-q = Queue()
-for i in tweets:
-    q.put(i)
-num_threads = 3
-threads = []
-for i in range(num_threads):
-    t = threading.Thread(target=thread)
-    t.daemon = True
-    threads.append(t)
-    t.start()
-q.join() 
+if __name__ == '__main__':
+    tweets = ["@BU_Tweets","@BUAthletics","@BUexperts"]
+    main(tweets)
 
