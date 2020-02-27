27cc170a3067253a06c79a374c10be9b1ff5dec9
szyszy315 <zeyusong@bu.edu>
2020-02-26 21:47:09 -0500

    Update test_video.py

--- test_video.py
+++ test_video.py
@@ -7,29 +7,28 @@ import json
 from subprocess import  check_output, CalledProcessError, STDOUT 
 import keys
 # test the content of json file
-def testcontentofjson(name):
+def contentofjson(name):
   tweets = []
   jfile = name + ".json"
   with open(jfile,"r") as file:
     tweets = json.load(file)
   if len(tweets) != 20:
-    print("error:didn't get enought tweets")
+    return ("error:didn't get enought tweets")
   for i in tweets:
     if isinstance(i, str) :
       continue
     else:
-      print('error:cotent of json file  is wrong')
-      return
-  print("content of tweets is right")
+      return ('error:cotent of json file  is wrong')
+  return ("content of tweets is right")
 
 # test if all neccessary file succesfully created
-def testifanyfilemiss(name):
-  print (name + ".json file exists: "+ str(os.path.exists(name + '.json')))
+def ifanyfilemiss(name):
   print (name + ".avi file exists: "+ str(os.path.exists(name + '.avi')))
   print(name + " folder exist: " + str(os.path.isdir(name)))
+  return (name + ".json file exists: "+ str(os.path.exists(name + '.json')))
 
 # test the lenght of input video
-def testdurationofvideo(name) :
+def durationofvideo(name) :
   filename = name + ".avi"
   command = [
       'ffprobe', 
@@ -42,26 +41,22 @@ def testdurationofvideo(name) :
       filename
     ]
   try:
-      output = check_output( command, stderr=STDOUT ).decode()
+    output = check_output( command, stderr=STDOUT ).decode()
   except CalledProcessError as e:
-      output = e.output.decode()
+    output = e.output.decode()
   duration = float(output)
   if int(duration) != 60:
-    print("the duration of video wrong")
-    return
-  print("video successfully generated")
+    return ("the duration of video wrong")  
+  return("video successfully generated")
 
-def test():
+def test_video():
   name = "@BU_Tweets"
-  if len(keys.CONSUMER_KEY) > 3 :
+  if (len(keys.CONSUMER_KEY) > 3) :
     # run the tested functions
     gettweet.get_all_tweets(name)
     texttoimage.image(name)
     image2video.video(name)
   #start the test
-  testcontentofjson(name)
-  testifanyfilemiss(name)
-  testdurationofvideo(name)
-
-if __name__ == '__main__':
-  test()
+  assert contentofjson(name) == "content of tweets is right"
+  assert ifanyfilemiss(name) == name + ".json file exists: True"
+  assert durationofvideo(name) == "video successfully generated"
